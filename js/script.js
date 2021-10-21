4c5657c5e117fd9d52f6c158d13ffff6fe20dc66
efm596065 <92921350+efm596065@users.noreply.github.com>
2021-10-21 19:21:45 +0900

    upload file website fotografer

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Best', 'Fotographer', 'In here']
+let count = 0;
+let textIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+	
+	if (count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++textIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+    if (words.length == currentTxt.length){
+    	count++;
+    	textIndex = 0;
+}
+
+	setTimeout(ngetik, 500);
+
+})();
